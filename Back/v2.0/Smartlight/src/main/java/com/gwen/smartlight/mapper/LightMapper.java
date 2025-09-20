abb9993c57d4157c9996debf34590e2c22fb3e35
MI <#481447414@qq.com>
2025-09-20 21:55:16 +0800

    2.0版本改名

--- Back/v2.0/Smartlight/src/main/java/com/gwen/smartlight/mapper/LightMapper.java
+++ Back/v2.0/Smartlight/src/main/java/com/gwen/smartlight/mapper/LightMapper.java
@@ -3,7 +3,7 @@ package com.gwen.smartlight.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.gwen.smartlight.entity.Light;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
+
 
 @Mapper
 public interface LightMapper extends BaseMapper<Light> {
